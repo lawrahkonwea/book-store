f80f51bf2b33d3c4040d57664e5095e64058df5e
Amaka Konwea <lawrahcollections@gmail.com>
2023-01-28 18:15:12 +0000

    Implemented changes

--- src/components/ContentBook.js
+++ src/components/ContentBook.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ContentBook = (props) => {
-  const { title, author } = props;
-  return (
-    <li>
-      <h3>{title}</h3>
-      <p>{author}</p>
-      <button type="button">Delete</button>
-    </li>
-  );
-};
+const ContentBook = ({ title, author }) => (
+  <li>
+    <h3>{title}</h3>
+    <p>{author}</p>
+    <button type="button">Delete</button>
+  </li>
+);
 ContentBook.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
